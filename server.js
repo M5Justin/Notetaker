d0c3e01673800ae10419d01f5bd6184b517035a6
justin moran <justinmoranre@gmail.com>
2023-01-05 21:07:36 -0500

    added port listener and route for homepage

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const util = require('util');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+
+
+// Get route for homepage
 app.get('/', (rec, res) => {
-    res.send('Hello class');
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-app.listen(PORT);
+
+
+
+
+
+
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
