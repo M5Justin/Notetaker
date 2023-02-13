6a68e866cbd09319c557721b09f6c9268fdb87dc
unknown <justinmoranre@gmail.com>
2023-02-13 10:42:12 -0500

    routed files from public dir

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
 const PORT = process.env.PORT || 3001;
@@ -12,10 +12,11 @@ app.use(express.json());
 // routes
 
 app.get('/', (req, res) => {
-
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 })
 
 app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 
 })
 
